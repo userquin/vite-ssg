3d5d43862dccccbeb4ede540a232d19cf65037bc
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-06-22 01:28:29 +0200

    feat: add I18nRouterLink

---
+++ src/client/components/I18nRouterLink.ts
@@ -0,0 +1,24 @@
+import { defineComponent, h } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { RouterLink } from 'vue-router'
+import { injectDefaultLocale } from '../../i18n/composables'
+
+export const I18nRouterLink = defineComponent({
+  inheritAttrs: false,
+  setup(_, { attrs, slots }) {
+    const { locale } = useI18n({ useScope: 'global' })
+    const defaultLocale = injectDefaultLocale()!
+    return () => {
+      let { to, ...useAttrs } = attrs
+
+      if (locale.value !== defaultLocale.locale) {
+        if (typeof to === 'string')
+          to = `/${locale.value}/${to.startsWith('/') ? to.substring(1) : to}`
+        else
+          to.path = `/${locale.value}/${to.path.startsWith('/') ? to.path.substring(1) : to.path}`
+      }
+
+      return h(RouterLink, { ...useAttrs, to }, slots)
+    }
+  },
+})
