c21c69cc1c24ad7943a6b3e62883bbaf00b71b2c
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-06-22 10:28:20 +0200

    chore: adjust `i18n` module, we are using `<i18n src='...'>` on components

--- examples/multiple-pages-i18n/src/modules/i18n.ts
+++ examples/multiple-pages-i18n/src/modules/i18n.ts
@@ -10,30 +10,27 @@ export const install = (ctx: ViteSSGContext) => {
   ctx.createI18n?.(
     ctx,
     globalMessages,
-    async(locale, to) => {
-      try {
-        const messagesModule = await import(/* @vite-ignore */ `../pages/${to.meta.rawI18nPath}.json5`)
-        // const messagesModule = await import(/* @vite-ignore */ `../../locales/pages/${to.meta.rawI18nPath}.json5`)
-        // should use default
-        return messagesModule.default || messagesModule
-      }
-      catch (e) {
-        console.error('uppps', e)
-        return undefined
-      }
-    },
-    (route, headObject, pageMessages, locale) => {
+    undefined,
+    // async(locale, to) => {
+    //   try {
+    //     const messagesModule = await import(/* @vite-ignore */ `../pages/${to.meta.rawI18nPath}.json5`)
+    //     // const messagesModule = await import(/* @vite-ignore */ `../../locales/pages/${to.meta.rawI18nPath}.json5`)
+    //     // should use default
+    //     return messagesModule.default || messagesModule
+    //   }
+    //   catch (e) {
+    //     console.error('uppps', e)
+    //     return undefined
+    //   }
+    // },
+    async(route, headObject, pageMessages, locale) => {
       const meta = route.meta
       if (meta && meta.injectI18nMeta) {
-        if (pageMessages) {
-          const key = `/${meta.rawI18nPath}`
-          if (pageMessages.te(`${key}.title`))
-            meta.title = pageMessages.t(`${key}.title`)
-          if (pageMessages.te(`${key}.description`))
-            meta.description = pageMessages.t(`${key}.description`)
-        }
-        meta.injectI18nMeta(headObject, locale)
+        // we can add what we want, also change the entire headObject
+        meta.injectI18nMeta(headObject.value, locale)
       }
+
+      return true
     },
   )
 }
